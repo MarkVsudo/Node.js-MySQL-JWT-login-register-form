ccaa2226012d2f779d04a5612794e61b0931ee78
Mark Veskov <mark.veskov@abv.bg>
2023-08-05 23:59:43 +0300

    Finished changes

--- routes/pages.js
+++ routes/pages.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const loggedIn = require("../controllers/loggedIn");
+const logout = require("../controllers/logout");
 const router = express.Router();
 
 router.get("/", loggedIn, (req, res) => {
-  res.render("index");
+  if (req.user) {
+    res.render("index", { status: "loggedIn", user: req.user });
+  } else {
+    res.render("index", { status: "no", user: "nothing" });
+  }
 });
 
 router.get("/register", (req, res) => {
@@ -14,4 +19,5 @@ router.get("/login", (req, res) => {
   res.sendFile("login.html", { root: "./public/" });
 });
 
+router.get("/logout", logout);
 module.exports = router;
